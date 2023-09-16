f5f5e9be53bac985533e493c148286a9842a80dc
LogSyn <[200~92345738+b-tess@users.noreply.github.com~>
2023-09-16 18:28:54 +0300

    backend & frontend connection attempt

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const app = e()
 app.use(e.json())
 app.use(
     cors({
-        origin: ['http://localhost:3000', 'https://stocktake-app.onrender.com'],
+        origin: ['http://localhost:3000', 'https://stocktake.onrender.com'],
         methods: 'GET',
         allowedHeaders: [
             'Content-Type',
