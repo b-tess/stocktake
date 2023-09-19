0664db9aa1ab494adf5e68ae5123a543f58c7b5f
Barbara Tess <barbaratessn@gmail.com>
2023-09-19 14:19:49 +0300

    expDate output format changed

--- frontend/src/components/StockItem.jsx
+++ frontend/src/components/StockItem.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom'
 
 function StockItem({ stockItem }) {
-    // const { page } = useParams()
+    // format the expiry date as month/year
+    let expiryDate = new Date(stockItem.expDate)
+    const month = expiryDate.getMonth() + 1
+    const year = expiryDate.getFullYear()
     return (
         <div className='stock-item'>
             <div>{stockItem.name}</div>
             <div>{stockItem.inStock}</div>
-            <div>
-                {new Date(stockItem.expDate).toLocaleString().slice(3, 10)}
-            </div>
+            <div>{`${month}/${year}`}</div>
             <div>{stockItem.itemType}</div>
             <Link
                 to={`/stockitem/${stockItem._id}`}
